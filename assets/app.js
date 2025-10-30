4d94a953617e9cc7aacc4d2f7637bd960359e340
sandeep-megaska <sandeep.lalu@gmail.com>
2025-10-30 16:51:46 +0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -925,7 +925,7 @@ async function wirePassPanelForPlayer(p) {
   const passPanel = document.getElementById("passPanel");
   if (passPanel) passPanel.scrollIntoView({ behavior: "smooth", block: "center" });
 });
-await wirePassPanelForPlayer(p);
+
 }
 
 function renderSelectedList(){
